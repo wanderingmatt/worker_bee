3520d222bacb9c1d6b78d1d781ea0be71c3e01d2
Matthew Anderson <manderson@onehub.com>
2009-04-27 20:50:00 -0700

    Refactored tests to account for new data structure ideas and rewrote code to pass.

--- lib/worker_bee.rb
+++ lib/worker_bee.rb
@@ -1,23 +1,40 @@
-class WorkerBee
+module WorkerBee
   VERSION = '0.0.1'
   
-  attr_accessor :completed_tasks
+  class Work
+    attr_accessor :name, :deps, :block, :completed
+
+    def initialize name, *deps, &block
+      @name = name
+      @deps = deps
+      @block = block
+      @completed = false
+    end
+  end
+      
+  @works = {}
   
-  def initialize
-    @completed_tasks = {}
+  def self.works
+    @works
+  end
+  
+  def self.recipe &block
+    raise ArgumentError, "Recipe needs a block" unless block_given?
+    module_eval &block
+  end
+  
+  def self.work name, *deps, &block
+    raise ArgumentError, "Work needs a block" unless block_given?
+    @works[name] = WorkerBee::Work.new deps, block
   end
   
   def self.run name
-    "running #{name.to_s}"
-  end
-  
-  def recipe &block
-    raise ArgumentError, "Recipe needs a block" unless block_given?
-    yield
-  end
-  
-  def work name, *deps, &block
-    raise ArgumentError, "Work needs a block" unless block_given?
-    yield
+    puts "running #{name.to_s}"
+    unless @work
+      #execute
+    else
+      puts "** #{name.to_s}!"
+    end
   end
 end
+
--- test/test_worker_bee.rb
+++ test/test_worker_bee.rb
@@ -3,37 +3,8 @@ require 'worker_bee'
 
 class TestWorkerBee < Test::Unit::TestCase
   def setup
-    @wb = WorkerBee.new
-  end
-  
-  def test_no_completed_tasks
-    @wb.recipe do
-    end
-    
-    assert @wb.completed_tasks.empty?
-  end
-  
-  def test_run_takes_a_name
-    expected = 'running sammich'
-    actual = WorkerBee.run :sammich
-    
-    assert_equal expected, actual
-  end
-  
-  def test_recipe_raises_without_a_block
-    assert_raise(ArgumentError) {
-      @wb.recipe
-    }
-  end
-  
-  def test_recipe_yields_a_block
-    expected = 'blocky'
-    actual = @wb.recipe do
-      'blocky'
-    end
-        
-    assert_equal expected, actual
-  end
+    @wb = WorkerBee
+  end  
   
   def test_work_raises_without_a_block
     assert_raise(ArgumentError) {
@@ -41,12 +12,26 @@ class TestWorkerBee < Test::Unit::TestCase
     }
   end
   
-  def test_work_yields_a_block
-    expected = 'blocky'
-    actual = @wb.work :name, :dep do
-      'blocky'
+  def test_work_creates_new_work
+    @wb.work :name do
+      puts "** name"
+    end
+    
+    assert @wb.works.key? :name
+  end
+  
+  
+  def test_recipe_raises_without_a_block
+    assert_raise(ArgumentError) {
+      @wb.recipe
+    }
+  end
+  
+  def test_recipe_evals_a_block
+    actual = @wb.recipe do
+      'block'
     end
         
-    assert_equal expected, actual
+    assert_equal 'block', actual
   end
 end
